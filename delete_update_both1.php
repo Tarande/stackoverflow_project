7cd9ca2fd30f5e7aab8ff557b250d1ad053efa60
Tarandeep singh <tarandeepsingh7837420@gmail.com>
2021-03-29 22:57:07 +0530

    added glyphicons  and gave hyperlinks to that

--- delete_update_both1.php
+++ delete_update_both1.php
@@ -36,9 +36,21 @@
      
      
      <div class="navbar-fixed-top">
+         <ul class="nav navbar-nav navbar-right">
+             <li><a href="update_question1.php"><span class="glyphicon glyphicon-info-sign"></span> UPDATE </a> </li>
+                    <li><a href="index.php"><span class="glyphicon glyphicon-user"></span> ENTER </a> </li>
+                    <li><a href=index1.php><span class="glyphicon glyphicon-search"></span> FETCH</a> </li>
+                    <li><a href="DELETE1.php"><span class="glyphicon glyphicon-trash"></span> DELETE</a> </li>
+                    
+                </ul>
    
     <div id="yo" class="img">
-            <h2> STACKOVERFLOW UPDATE</h2> 
+            <h2>        <span class="glyphicon glyphicon-globe"></span>STACKOVERFLOW UPDATE</h2> 
+            
+            
+            
+            
+            
     
         
     </div>
